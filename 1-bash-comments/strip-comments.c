520fadcf7b5a02968ad39a35e91a4af2ae094473
Jada Probst <jadawp@unc.edu>
2026-09-07 21:50:36 +0000

    Step 1, attempt 1

--- 1-bash-comments/strip-comments.c
+++ 1-bash-comments/strip-comments.c
@@ -2,9 +2,34 @@
 #include <stdlib.h>
 
 int main() {
+    int in_single = 0, in_double = 0;
+    int prev = '\n';
     int c;
     while ((c = getchar()) != EOF) {
+        if (!in_single && !in_double && c == '#' &&
+            (prev == '\n' || prev == ' ' || prev == '\t')) {
+            while ((c = getchar()) != EOF && c != '\n');
+            if (c == EOF) break;
+            putchar('\n');
+            prev = '\n';
+            continue;
+        }
+
+        if (c == '\'' && in_single) {
+            putchar(c);
+            if ((c = getchar()) == EOF) break;
+            putchar(c);
+            prev = 'x';
+            continue;
+        }
+
+        if (c == '\'' && !in_double)
+            in_single = !in_single;
+        else if (c == '"' && !in_single)
+            in_double = !in_double;
+
         putchar(c);
+        prev = c;
     }
     return EXIT_SUCCESS;
 }

dfc5fe09e50a5bd8ec2a13fc61d1de20c56220ce
Jada Probst <jadawp@unc.edu>
2026-09-07 21:17:24 +0000

    Step 0, attempt 2

--- 0-hello-world/hello.c
+++ 0-hello-world/hello.c
@@ -2,9 +2,10 @@
 // I pledge the Comp211 honor code.
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("hello, world!\n");
+    printf("hello, world\n");
     printf("welcome to c!\n");
-    return 0;
+    return EXIT_SUCCESS;
 }
